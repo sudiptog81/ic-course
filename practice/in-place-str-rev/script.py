111df992ffb94388434a7be86f1806a7859ef520
Sudipto Ghosh <sudipto@ghosh.pro>
2024-04-25 10:08:15 +0000

    initial commit

---
+++ practice/in-place-str-rev/script.py
@@ -0,0 +1,52 @@
+import unittest
+
+
+def reverse(list_of_chars):
+
+    # Reverse the input list of chars in place
+    for i in range(len(list_of_chars) // 2):
+        list_of_chars[i], list_of_chars[len(list_of_chars) - 1 - i] = list_of_chars[len(list_of_chars) - 1 - i], list_of_chars[i]
+    
+    return list_of_chars
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_empty_string(self):
+        list_of_chars = []
+        reverse(list_of_chars)
+        expected = []
+        self.assertEqual(list_of_chars, expected)
+
+    def test_single_character_string(self):
+        list_of_chars = ['A']
+        reverse(list_of_chars)
+        expected = ['A']
+        self.assertEqual(list_of_chars, expected)
+
+    def test_longer_string(self):
+        list_of_chars = ['A', 'B', 'C', 'D', 'E']
+        reverse(list_of_chars)
+        expected = ['E', 'D', 'C', 'B', 'A']
+        self.assertEqual(list_of_chars, expected)
+
+
+unittest.main(verbosity=2)
